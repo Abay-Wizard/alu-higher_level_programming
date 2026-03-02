c748211550c592f6294f87c0046931b41ea5df49
Abay-Wizard <a.tessema@alustudent.com>
2026-03-02 02:23:59 -0800

    done

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import hidden_4
+from hidden_4 import *
 
 if __name__ == "__main__":
-    names = dir(hidden_4)
+    names = dir()
     for name in sorted(names):
         if not name.startswith("__"):
             print(name)
